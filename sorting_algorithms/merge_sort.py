19f93330ec80ce8cd7fd00c9a2d1a29a6ecf9987
D1N3SH_ <tomekjelito@gmail.com>
2021-10-01 10:31:01 +0200

    merge sort 1.1

--- sorting_algorithms/merge_sort.py
+++ sorting_algorithms/merge_sort.py
@@ -3,39 +3,42 @@
 # https://github.com/D1N3SHh/algorithms
 
 
-list = [3, 7, 1, 8, 4, 6, 9, 2, 5]
-
-
-def merge(list, p, q, r):
+def merge(A, p, q, r):
     n1 = q - p + 1
     n2 = r - q
     left = [0] * (n1 + 1)
     right = [0] * (n2 + 1)
     for i in range(0, n1):
-        left[i] = list[p + i]
+        left[i] = A[p + i]
     for j in range(0, n2):
-        right[j] = list[q + j + 1]
+        right[j] = A[q + j + 1]
     left[n1] = 2147483647           # guard cards
     right[n2] = 2147483647          #
     i = 0
     j = 0
     for k in range(p, r + 1):
         if left[i] <= right[j]:
-            list[k] = left[i]
+            A[k] = left[i]
             i += 1
         else:
-            list[k] = right[j]
+            A[k] = right[j]
             j = j + 1
 
 
-def merge_sort(list, p, r):
+def merge_sort(A, p, r):
     if p < r:
         q = (p + r) // 2
-        merge_sort(list, p, q)
-        merge_sort(list, q + 1, r)
-        merge(list, p, q, r)
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+    return A
+
+
+def example_usage():
+    A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
+    print('Unsorted:\t', A)
+    print('Sorted:\t\t', merge_sort(A, 0, len(A) - 1))
 
 
 if __name__ == "__main__":
-    merge_sort(list, 0, len(list) - 1)
-    print(list)
+    example_usage() # run an example on start
