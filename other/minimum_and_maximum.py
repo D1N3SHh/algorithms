af3295d793dae133bfe741a2652c0311fd6097ad
D1N3SH_ <tomekjelito@gmail.com>
2021-10-01 09:21:42 +0200

    min and max 1.1

--- other/minimum_and_maximum.py
+++ other/minimum_and_maximum.py
@@ -3,15 +3,11 @@
 # https://github.com/D1N3SHh/algorithms
 
 
-A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
-
-
 def minimum(A):
     min = A[0]
     for i in range(1, len(A)):
         if min > A[i]:
             min = A[i]
-
     return min
 
 
@@ -20,9 +16,13 @@ def maximum(A):
     for i in range(1, len(A)):
         if max < A[i]:
             max = A[i]
-
     return max
 
-if __name__ == "__main__":
+def example_usage():
+    A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
     print("Minimum: ", minimum(A))
     print("Maximum: ", maximum(A))
+
+
+if __name__ == "__main__":
+    example_usage() # run an example on start
