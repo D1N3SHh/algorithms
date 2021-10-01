619ba4396ad9c4a9b0a0306872ee6abe70ed0d9f
D1N3SH_ <tomekjelito@gmail.com>
2021-10-01 10:19:15 +0200

    heap sort 1.1

--- sorting_algorithms/heap_sort.py
+++ sorting_algorithms/heap_sort.py
@@ -3,41 +3,44 @@
 # https://github.com/D1N3SHh/algorithms
 
 
-list = [3, 7, 1, 8, 4, 6, 9, 2, 5]
-
-
-def max_heapify(list, i, heap_size):
+def max_heapify(A, i, heap_size):
     left = 2*i+1
     right = 2*i+2
     largest = i
 
-    if left < heap_size and list[left] > list[largest]:
+    if left < heap_size and A[left] > A[largest]:
         largest = left
-    if right < heap_size and list[right] > list[largest]:
+    if right < heap_size and A[right] > A[largest]:
         largest = right
 
     if largest != i:
-        buf = list[i]
-        list[i] = list[largest]
-        list[largest] = buf
-        max_heapify(list, largest, heap_size)
+        buf = A[i]
+        A[i] = A[largest]
+        A[largest] = buf
+        max_heapify(A, largest, heap_size)
 
 
-def bild_max_heap(list, heap_size):
+def bild_max_heap(A, heap_size):
     for i in range(heap_size // 2 - 1, -1, -1):
-        max_heapify(list, i, heap_size)
+        max_heapify(A, i, heap_size)
 
 
-def heap_sort(list):
-    heap_size = len(list)
-    bild_max_heap(list, heap_size)
+def heap_sort(A):
+    heap_size = len(A)
+    bild_max_heap(A, heap_size)
     for i in range(heap_size - 1, 0, -1):
-        buf = list[0]
-        list[0] = list[i]
-        list[i] = buf
-        max_heapify(list, 0, i)
+        buf = A[0]
+        A[0] = A[i]
+        A[i] = buf
+        max_heapify(A, 0, i)
+    return A
+
+
+def example_usage():
+    A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
+    print('Unsorted:\t', A)
+    print('Sorted:\t\t', heap_sort(A))
 
 
 if __name__ == "__main__":
-    heap_sort(list)
-    print(list)
+    example_usage() # run an example on start
