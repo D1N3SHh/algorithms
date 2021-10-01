872e1c54ceb7ad0881199a82cdaaa8c3da582e87
D1N3SH_ <tomekjelito@gmail.com>
2021-10-01 09:42:48 +0200

    quick sort 1.1

--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -6,9 +6,6 @@
 import random
 
 
-A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
-
-
 def randomized_partition(A, p, r):
     i = random.randint(p, r)
     A[r], A[i] = A[i], A[r]
@@ -20,6 +17,7 @@ def randomized_quick_sort(A, p, r):
         q = randomized_partition(A, p, r)
         randomized_quick_sort(A, p, q - 1)
         randomized_quick_sort(A, q + 1, r)
+    return A
 
 
 def partition(A, p, r):
@@ -39,8 +37,14 @@ def quick_sort(A, p, r):
         q = partition(A, p, r)
         quick_sort(A, p, q - 1)
         quick_sort(A, q + 1, r)
+    return A
+
+
+def example_usage():
+    A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
+    print('Unsorted:\t', A)
+    print('Sorted:\t\t', quick_sort(A, 0, len(A) - 1))
 
 
 if __name__ == "__main__":
-    randomized_quick_sort(A, 0, len(A) - 1)
-    print(A)
+    example_usage() # run an example on start
