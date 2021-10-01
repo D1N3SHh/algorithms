ffc126e715c157f7e90123d5b8f890b27b9502c8
D1N3SH_ <tomekjelito@gmail.com>
2021-10-01 12:55:41 +0200

    bubble sort 1.2

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -3,20 +3,18 @@
 # https://github.com/D1N3SHh/algorithms
 
 
-def bubble_sort(list):
-    for i in range(0, len(list)):
-        for j in range(len(list) - 1, i, -1):
-            if list[j] < list[j - 1]:
-                buf = list[j - 1]
-                list[j - 1] = list[j]
-                list[j] = buf
-    return list
+def bubble_sort(A):
+    for i in range(0, len(A)):
+        for j in range(len(A) - 1, i, -1):
+            if A[j] < A[j - 1]:
+                A[j - 1], A[j] = A[j], A[j - 1]
+    return A
 
 
 def example_usage():
-    list = [3, 7, 1, 8, 4, 6, 9, 2, 5]
-    print('Unsorted:\t', list)
-    print('Sorted:\t\t', bubble_sort(list))
+    A = [3, 7, 1, 8, 4, 6, 9, 2, 5]
+    print('Unsorted:\t', A)
+    print('Sorted:\t\t', bubble_sort(A))
 
 
 if __name__ == "__main__":
